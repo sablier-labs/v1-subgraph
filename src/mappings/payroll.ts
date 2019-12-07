ac209354b2a473ea6e6c16052eee7bb1944feb8e
Paul Razvan Berg <hello@paulrberg.com>
2019-12-07 16:29:40 +0200

    refactor: rename Salary to ProxyStream

--- src/mappings/payroll.ts
+++ src/mappings/payroll.ts
@@ -1,16 +1,22 @@
-import { Salary, Stream } from "../types/schema";
+import { ProxyStream, Stream } from "../types/schema";
 import { CreateSalary as CreateSalaryEvent } from "../types/Payroll/Payroll";
 
+/**
+ * Maps the salary creation event to an object called "proxyStream". You may notice a naming
+ * discrepancy, but this is on purpose. Initially, we thought that the proxy would only be
+ * useful to the payroll dapp, only to later realise that a proxy that can be used to run a
+ * relayer system would be pretty damn useful.
+ */
 export function handleCreateSalary(event: CreateSalaryEvent): void {
   let stream = Stream.load(event.params.streamId.toString());
   if (stream == null) {
     return;
   }
 
-  let salary = new Salary(event.params.salaryId.toString());
-  salary.company = event.params.company;
-  salary.employee = stream.recipient;
-  salary.stream = event.params.streamId.toString();
-  salary.timestamp = event.block.timestamp;
-  salary.save();
+  let proxyStream = new ProxyStream(event.params.salaryId.toString());
+  proxyStream.sender = event.params.company;
+  proxyStream.recipient = stream.recipient;
+  proxyStream.stream = event.params.streamId.toString();
+  proxyStream.timestamp = event.block.timestamp;
+  proxyStream.save();
 }
